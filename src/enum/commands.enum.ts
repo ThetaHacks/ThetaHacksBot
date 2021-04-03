0a4cd4947c207d90997ce9699591b9c7a230ef56
Kai Devrim <kkgithub@devrim.tech>
2021-04-03 16:11:48 -0700

    Update commands.enum.ts

--- src/enum/commands.enum.ts
+++ src/enum/commands.enum.ts
@@ -1,26 +1,16 @@
 export enum COMMANDS {
-  info = "Get information about ScribeHacks and the bot.",
+  info = "Get information about ThetaHacks and the bot.",
   help = "This command lol. You can use !help info and !help fun.",
   next = "Information about the next event including a countdown and overall description",
   signup = "Get the link to signup for the Hackathon",
   ping = "Get latency of the bot",
-  tickle = "Tickle another user. Must @ another user",
-  cuddle = "Cuddle another user. Must @ another user",
-  poke = "Poke another user. Must @ another user",
-  feed = "Feed another user. Must @ another user",
   dice = "Roll a dice",
   magic8 = "Get what the magic 8 ball says",
   kill = "Kill another user. Uh Oh",
-  kiss = "Sends a kiss to another user. Must @ another user.",
-  smug = "Be smug",
-  baka = "You are a baka",
-  pat = "Pat another user. Must @ another user",
-  hug = "Hug another user. Must @ another user",
-  slap = "Slap another user. Must @ another user",
 }
 
 export enum COMMANDS_INFO {
-  info = "Get information about ScribeHacks and the bot.",
+  info = "Get information about ThetaHacks and the bot.",
   help = "This command lol. You can use !help info and !help fun.",
   next = "Information about the next event including a countdown and overall description",
   signup = "Get the link to signup for the Hackathon",
@@ -28,22 +18,13 @@ export enum COMMANDS_INFO {
 }
 
 export enum COMMANDS_FUN_1 {
-  tickle = "Tickle another user. Must @ another user",
-  cuddle = "Cuddle another user. Must @ another user",
-  poke = "Poke another user. Must @ another user",
-  feed = "Feed another user. Must @ another user",
   dice = "Roll a dice",
   magic8 = "Get what the magic 8 ball says",
   kill = "Kill another user. Uh Oh"
 }
 
 export enum COMMANDS_FUN_2 {
-  kiss = "Sends a kiss to another user. Must @ another user.",
-  smug = "Be smug",
-  baka = "You are a baka",
-  pat = "Pat another user. Must @ another user",
-  hug = "Hug another user. Must @ another user",
-  slap = "Slap another user. Must @ another user",
+  test = "how"
 }
 
 export enum ADMIN_COMMANDS {
